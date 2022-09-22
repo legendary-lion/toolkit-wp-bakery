cd12c73cbdcbfb8ca07bb82d59da35ab0cf6f6c6
Josh Watson <josh@legendarylion.com>
2022-09-22 11:43:04 -0400

    Corrected vc default css param output

--- toolkit-wp-bakery.php
+++ toolkit-wp-bakery.php
@@ -3,7 +3,7 @@
  * Plugin Name: WPBakery Toolkit
  * Plugin URI: https://legendarylion.com
  * Description: Modifies the options within WPBakery
- * Version: 1.0.14
+ * Version: 1.0.15
  * Author: Legendary Lion
  * Author URI: https://legendarylion.com
  */
